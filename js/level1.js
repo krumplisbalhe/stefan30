697360b13fabbccc77ca1eb790c11c093c117e9a
rita.radovics <rita.radovics@agillic.com>
2019-06-21 01:33:08 +0200

    First commit

---
+++ js/level1.js
@@ -0,0 +1,158 @@
+const stefan = document.getElementById('stefan')
+const speechBubble = document.getElementById('speechBubble')
+const speechP = document.getElementById('speechP')
+const lightBulb = document.getElementById('lightBulb')
+const level = document.getElementById('level')
+const SkiFree = document.getElementById('SkiFree')
+const level1Container = document.getElementById('level1Container')
+const pernille = document.getElementById('pernille')
+
+
+
+window.onload = () => sequence()
+
+const waiter = delay => new Promise(resolve => setTimeout(resolve, delay))
+
+const sequence = async () => {
+  await showStefan()
+  await waiter(500)
+  await startSpeaking()
+	await speechBubbleAppear()
+	await speechBubbleTextChange()
+	await speechBubbleTextChange2()
+	await speechBubbleTextChange3()
+	await speechBubbleTextChange4()
+	await stefanStopsSpeaking()
+	await stefanIdea()
+	await startGame()
+}
+
+const showStefan = () => {
+  return new Promise(resolve => {
+    TweenLite.to(stefan, 2, {
+      bottom: '20px',
+      onComplete: resolve
+    })
+  })
+}
+
+const startSpeaking = () => {
+  stefan.style.backgroundImage = 'url(../Characters/Stefan/StefanSpeaking.gif)'
+  return waiter(500)
+}
+
+const speechBubbleAppear = () => {
+  speechBubble.style.opacity = '1'
+  return waiter(200)
+}
+
+const speechBubbleTextChange = () => {
+  speechP.innerHTML = 'It\'s my 30th birthday'
+  return waiter(200)
+}
+
+const speechBubbleTextChange2 = () => {
+  speechP.innerHTML = 'and I have so much to do!'
+  return waiter(200)
+}
+
+const speechBubbleTextChange3 = () => {
+  speechP.innerHTML = 'Help me solve the tasks'
+  return waiter(200)
+}
+
+const speechBubbleTextChange4 = () => {
+	speechP.innerHTML = 'and rescue the Princess!'
+	return waiter(200)
+}
+
+const stefanStopsSpeaking = () => {
+	stefan.style.backgroundImage = 'url(../Characters/Stefan/Stefan1.png)'
+	speechBubble.style.opacity = '0'
+	speechBubble.pointerEvents = 'none'
+	return waiter(100)
+}
+
+const stefanIdea = () => {
+	TweenLite.to(lightBulb, 0.5, {opacity: 1, onComplete:function(){
+		TweenMax.to(this.target, 0.5, {boxShadow:'0px 0px 10px 5px rgba(255,255,255)',repeat:-1, yoyo: true})
+	}})
+	stefan.style.backgroundImage = 'url(../Characters/Stefan/StefanScreaming.png)'
+	lightBulb.style.cursor = 'pointer'
+}
+
+const startGame = () => {
+	lightBulb.addEventListener('click', levelOne)
+}
+
+const levelOne = () => {
+	stefan.style.backgroundImage = 'url(../Characters/Stefan/StefanWorried.gif)'
+	level.style.opacity = '1'
+	lightBulb.style.opacity = '0'
+	lightBulb.style.pointerEvents = 'none'
+	speechBubble.style.opacity = '1'
+	speechP.innerHTML = 'Oh no! My sister is in'
+	setTimeout(speechBubbleTextChange5, 200)
+}
+
+const speechBubbleTextChange5 = () => {
+	speechP.innerHTML = 'trouble! She is stuck in'
+	setTimeout(speechBubbleTextChange6, 200)
+}
+
+const speechBubbleTextChange6 = () => {
+	speechP.innerHTML = 'the PC room, I have to'
+	setTimeout(speechBubbleTextChange7, 200)
+}
+
+const speechBubbleTextChange7 = () => {
+	speechP.innerHTML = 'save her!'
+	stefan.style.backgroundImage = 'url(../Characters/Stefan/StefanScreaming.png)'
+	setTimeout(playFreeSki, 200)
+}
+
+const playFreeSki = () => {
+	level1Container.style.backgroundImage = 'none'
+	level1Container.style.backgroundColor = 'white'
+	stefan.style.opacity = '0'
+	stefan.style.pointerEvents = 'none'
+	speechP.style.opacity = '1'
+	speechP.style.pointerEvents = 'none'
+	speechBubble.style.opacity = '0'
+	speechBubble.style.pointerEvents = 'none'
+	SkiFree.style.opacity = '1'
+	SkiFree.style.pointerEvents = 'auto'
+	pernille.style.opacity = '1'
+	speechBubble.style.opacity = '1'
+	speechBubble.style.right = '0px'
+	speechP.innerHTML = 'Play 30 seconds without'
+	setTimeout(speechBubbleTextChange8, 2000)
+}
+
+const speechBubbleTextChange8 = () => {
+	speechP.innerHTML = 'loosing and I\'m free!'
+	pernille.style.backgroundImage = 'url(../Characters/Pernille/PernilleSmile.png)'
+	setTimeout(pernilleStopsSpeaking, 2000)
+}
+
+startTimer = (duration, display) => {
+	var timer = duration, minutes, seconds;
+	setInterval(function () {
+			minutes = parseInt(timer / 60, 10)
+			seconds = parseInt(timer % 60, 10);
+			minutes = minutes < 10 ? "0" + minutes : minutes;
+			seconds = seconds < 10 ? "0" + seconds : seconds;
+			display.textContent = minutes + ":" + seconds;
+			if (--timer < 0) {
+					timer = 0;
+			}
+	}, 1000);
+}
+
+const pernilleStopsSpeaking = () => {
+	speechP.style.opacity = '0'
+	speechBubble.style.opacity = '0'
+	var halfMinute = 60 * 0.5,
+	display = document.querySelector('#time');
+	startTimer(halfMinute, display);
+}
