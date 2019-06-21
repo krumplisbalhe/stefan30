d47e7c0a3ee6c7f357808530ee5f35cae8d751ed
rita.radovics <rita.radovics@agillic.com>
2019-06-22 00:43:47 +0200

    level 1 somewhat ready

--- js/level1.js
+++ js/level1.js
@@ -6,8 +6,9 @@ const level = document.getElementById('level')
 const SkiFree = document.getElementById('SkiFree')
 const level1Container = document.getElementById('level1Container')
 const pernille = document.getElementById('pernille')
+const timeContainer = document.getElementById('timeContainer')
 
-
+const timeLength = 500
 
 window.onload = () => sequence()
 
@@ -38,39 +39,39 @@ const showStefan = () => {
 
 const startSpeaking = () => {
   stefan.style.backgroundImage = 'url(../Characters/Stefan/StefanSpeaking.gif)'
-  return waiter(500)
+  return waiter(timeLength)
 }
 
 const speechBubbleAppear = () => {
   speechBubble.style.opacity = '1'
-  return waiter(200)
+  return waiter(timeLength*4)
 }
 
 const speechBubbleTextChange = () => {
   speechP.innerHTML = 'It\'s my 30th birthday'
-  return waiter(200)
+  return waiter(timeLength*4)
 }
 
 const speechBubbleTextChange2 = () => {
   speechP.innerHTML = 'and I have so much to do!'
-  return waiter(200)
+  return waiter(timeLength*4)
 }
 
 const speechBubbleTextChange3 = () => {
   speechP.innerHTML = 'Help me solve the tasks'
-  return waiter(200)
+  return waiter(timeLength*4)
 }
 
 const speechBubbleTextChange4 = () => {
 	speechP.innerHTML = 'and rescue the Princess!'
-	return waiter(200)
+	return waiter(timeLength*4)
 }
 
 const stefanStopsSpeaking = () => {
 	stefan.style.backgroundImage = 'url(../Characters/Stefan/Stefan1.png)'
 	speechBubble.style.opacity = '0'
 	speechBubble.pointerEvents = 'none'
-	return waiter(100)
+	return waiter(timeLength*2)
 }
 
 const stefanIdea = () => {
@@ -92,23 +93,23 @@ const levelOne = () => {
 	lightBulb.style.pointerEvents = 'none'
 	speechBubble.style.opacity = '1'
 	speechP.innerHTML = 'Oh no! My sister is in'
-	setTimeout(speechBubbleTextChange5, 200)
+	setTimeout(speechBubbleTextChange5, timeLength*4)
 }
 
 const speechBubbleTextChange5 = () => {
 	speechP.innerHTML = 'trouble! She is stuck in'
-	setTimeout(speechBubbleTextChange6, 200)
+	setTimeout(speechBubbleTextChange6, timeLength*4)
 }
 
 const speechBubbleTextChange6 = () => {
 	speechP.innerHTML = 'the PC room, I have to'
-	setTimeout(speechBubbleTextChange7, 200)
+	setTimeout(speechBubbleTextChange7, timeLength*4)
 }
 
 const speechBubbleTextChange7 = () => {
 	speechP.innerHTML = 'save her!'
 	stefan.style.backgroundImage = 'url(../Characters/Stefan/StefanScreaming.png)'
-	setTimeout(playFreeSki, 200)
+	setTimeout(playFreeSki, timeLength*4)
 }
 
 const playFreeSki = () => {
@@ -116,23 +117,28 @@ const playFreeSki = () => {
 	level1Container.style.backgroundColor = 'white'
 	stefan.style.opacity = '0'
 	stefan.style.pointerEvents = 'none'
-	speechP.style.opacity = '1'
-	speechP.style.pointerEvents = 'none'
 	speechBubble.style.opacity = '0'
 	speechBubble.style.pointerEvents = 'none'
 	SkiFree.style.opacity = '1'
 	SkiFree.style.pointerEvents = 'auto'
+	setTimeout(speechBubbleTextChange8, timeLength*2)
+}
+
+const speechBubbleTextChange8 = () =>{
+	speechP.style.opacity = '1'
+	speechP.style.pointerEvents = 'none'
 	pernille.style.opacity = '1'
 	speechBubble.style.opacity = '1'
 	speechBubble.style.right = '0px'
-	speechP.innerHTML = 'Play 30 seconds without'
-	setTimeout(speechBubbleTextChange8, 2000)
+	speechP.innerHTML = 'Play FreeSki for 30 seconds'
+	setTimeout(speechBubbleTextChange9, timeLength*4)
 }
 
-const speechBubbleTextChange8 = () => {
-	speechP.innerHTML = 'loosing and I\'m free!'
+
+const speechBubbleTextChange9 = () => {
+	speechP.innerHTML = 'and I\'m free!'
 	pernille.style.backgroundImage = 'url(../Characters/Pernille/PernilleSmile.png)'
-	setTimeout(pernilleStopsSpeaking, 2000)
+	setTimeout(pernilleStopsSpeaking, timeLength*4)
 }
 
 startTimer = (duration, display) => {
@@ -145,6 +151,7 @@ startTimer = (duration, display) => {
 			display.textContent = minutes + ":" + seconds;
 			if (--timer < 0) {
 					timer = 0;
+					levelCompleted()
 			}
 	}, 1000);
 }
@@ -152,7 +159,14 @@ startTimer = (duration, display) => {
 const pernilleStopsSpeaking = () => {
 	speechP.style.opacity = '0'
 	speechBubble.style.opacity = '0'
-	var halfMinute = 60 * 0.5,
-	display = document.querySelector('#time');
-	startTimer(halfMinute, display);
+	timeContainer.style.opacity = '1'
+	var halfMinute = 6 * 0.5,
+	display = document.querySelector('#time')
+	startTimer(halfMinute, display)
+}
+
+levelCompleted = () => {
+	timeContainer.style.opacity = '0'
+	level.textContent = 'LEVEL COMPLETED'
+	level.style.left = '38%'
 }
